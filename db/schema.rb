7d447ce2c7b6565ac7f51616b0959d0752f613cf
sosuke <sosuke.sin@gmail.com>
2019-12-12 11:02:09 +0900

    DBよりURLカラムを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_11_053916) do
+ActiveRecord::Schema.define(version: 2019_12_12_020003) do
 
   create_table "choices", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
-    t.text "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
