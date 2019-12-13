359db53ef613e18fd6bd9e5f109cedd0266e88a0
sosuke <sosuke.sin@gmail.com>
2019-12-13 15:55:16 +0900

    turbolinksを無効にする

--- app/controllers/choices_controller.rb
+++ app/controllers/choices_controller.rb
@@ -1,15 +1,6 @@
 class ChoicesController < ApplicationController
   def new
     randum_num = Choice.pluck(:id).shuffle[0..9]
-    binding.pry
     @choices = Choice.find(randum_num)
-    # @choices.shuffle!
-
-
-    #   @choices = Array.new
-    #   if choice.id % 5 == 0
-    #     @choices.push(choice)
-    #   end
-    # end
   end
 end
